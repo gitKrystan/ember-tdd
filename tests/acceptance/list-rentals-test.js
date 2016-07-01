20b6295bac3c64b8dd1dab507e0894aa545f0821
Krystan Menne <kmenne@gmail.com>
2016-06-30 18:46:29 -0700

    add initial tests

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,38 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'ember-rentals/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | homepage');
+
+test('lists available rentals.', function (assert) {
+  visit('/');
+  andThen(function () {
+    assert.equal(find('.listing').length, 3, 'should see 3 listings');
+  });
+});
+
+test('links to info about the company.', function (assert) {
+  visit('/');
+  click('a:contains("About")');
+  andThen(function () {
+    assert.equal(currentURL(), '/about', 'should navigate to about');
+  });
+});
+
+test('links to contact info.', function (assert) {
+  visit('/');
+  click('a:contains("Contact")');
+  andThen(function () {
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
+  });
+});
+
+test('filters the list of rentals by city.', function (assert) {
+  visit('/');
+  fillIn('.list-filter input', 'seattle');
+  keyEvent('.list-filter input', 'keyup', 69); // 'e'
+  andThen(function () {
+    assert.equal(find('.listing').length, 1, 'should show 1 listing');
+    assert.equal(find('.listing .location:contains("Seattle")').length, 1,
+      'should contain 1 listing with location Seattle');
+  });
+});
